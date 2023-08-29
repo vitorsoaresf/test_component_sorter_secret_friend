5a90d9b6e4ede6c4940533d8cac3b1bf0ab2e25e
Vitor Soares <vitorsoaresf@gmail.com>
2023-08-29 08:33:09 -0300

    feat: implements new unit test submit form

--- src/components/Formulario.spec.tsx
+++ src/components/Formulario.spec.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { Formulario } from "./Formulario";
+import userEvent from "@testing-library/user-event";
 
 test("quando o input esta vazio, novos participantes nao podem ser adicionados", () => {
   render(<Formulario />);
@@ -13,4 +14,21 @@ test("quando o input esta vazio, novos participantes nao podem ser adicionados",
 
   expect(input).toBeInTheDocument();
   expect(bt).toBeDisabled();
+});
+
+test("adicionar um participante caso exista um nome preenchido", () => {
+  render(<Formulario />);
+
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+
+  const bt = screen.getByRole("button");
+
+  // fiveEvent eh uma opcao tbm
+  userEvent.type(input, "vitor");
+  userEvent.click(bt);
+
+  expect(input).toHaveFocus();
+  expect(input).toHaveValue("");
 });
